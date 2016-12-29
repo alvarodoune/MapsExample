914e65efc7fce2693f9b05a39244270e7ecfa3e8
alvarodoune <alvarodoune@gmail.com>
2016-12-29 01:02:21 -0300

    fix android key

--- MapsExample/MapsExamplePage.xaml.cs
+++ MapsExample/MapsExamplePage.xaml.cs
@@ -12,7 +12,7 @@ namespace MapsExample
 
 			MyMap.MoveToRegion(
 			MapSpan.FromCenterAndRadius(
-					new Position(37, -122), Distance.FromKilometers(5))
+					new Position(-32, -56), Distance.FromKilometers(1))
 			);
 
 			var pin = new Pin
@@ -22,13 +22,27 @@ namespace MapsExample
 				Label = "The Label"
 			};
 
+			var pin2 = new Pin
+			{
+				Position = new Position(-32.4545677, -56.2026182),
+				Address = "Adress 2",
+				Label = "The Label 2"
+			};
+
 			pin.Clicked += (sender, e) =>
 			{
 				var selectedPin = sender as Pin;
 				DisplayAlert(selectedPin.Label, selectedPin.Address, "OK");
 			};
 
+			pin2.Clicked += (sender, e) =>
+			{
+				var selectedPin = sender as Pin;
+				DisplayAlert(selectedPin.Label, selectedPin.Address, "OK");
+			};
+
 			MyMap.Pins.Add(pin);
+			MyMap.Pins.Add(pin2);
 		}
 	}
 }
